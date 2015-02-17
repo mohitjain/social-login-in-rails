ceebe8c4d2d5be3c232f2bdd66b2d1433986ef8f
Max Holzheu <maxhm10@gmail.com>
2015-02-16 21:02:05 -0600

    Add more clarity to User model. Remove duplication

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,13 +22,17 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth, current_user)
     authorization = Authorization.where(:provider => auth.provider, :uid => auth.uid.to_s, :token => auth.credentials.token, :secret => auth.credentials.secret).first_or_initialize
     if authorization.user.blank?
-      user = current_user.nil? ? User.where('email = ?', auth["info"]["email"]).first : current_user
+      user = current_user || User.where('email = ?', auth["info"]["email"]).first
       if user.blank?
        user = User.new
        user.password = Devise.friendly_token[0,10]
        user.name = auth.info.name
        user.email = auth.info.email
-       auth.provider == "twitter" ?  user.save(:validate => false) :  user.save
+       if auth.provider == "twitter" 
+         user.save(:validate => false) 
+       else
+         user.save
+       end
      end
      authorization.username = auth.info.nickname
      authorization.user_id = user.id
